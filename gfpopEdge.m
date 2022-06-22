9387db0587feee9b6c9d790f8563a54f8827d601
nat-sweeney <100242406+nat-sweeney@users.noreply.github.com>
2022-06-22 15:39:10 -0600

    Updated to work with specifying input variables

--- gfpopEdge.m
+++ gfpopEdge.m
@@ -1,29 +1,65 @@
 %%% Takes in edge values to create an edge to be put into gfpopGraph.m
 %%%
 %%%     INPUTS:
-%%%     state1 ---> string of first state for the edge
-%%%     state2 ---> string of second state for the edge
-%%%     edge type ---> string of the edge type for the edge
+%%%     [Required]
+%%%     state1 --> string of first state for the edge
+%%%     state2 --> string of second state for the edge
+%%%     edge type --> string of the edge type for the edge
 %%%         edge types: null <- u(t) == u(t+1)
 %%%                     std <- u(t) != u(t+1)
 %%%                     up <- u(t) + gap < u(t+1)
 %%%                     down <- u(t) > u(t+1) + gap
 %%%                     abs <- |u(t+1)-u(t)| > gap
-%%%     parameter ---> double of the parameters for the edge
-%%%     penalty ---> nonnegative double of the penalty for the edge
-%%%     K ---> positive double of the robust biweight gaussian loss
-%%%     a ---> positive double of the slope for the huber robust loss
-%%%     min ---> nonnegative double of the minimum gap for the edge
-%%%     max ---> nonnegative double of the maximum gap for the edge
+%%%     [Optional]
+%%%     parameter --> double of the parameters for the edge.
+%%%     penalty --> nonnegative double of the penalty for the edge.
+%%%     K --> positive double of the robust biweight gaussian loss
+%%%     a --> positive double of the slope for the huber robust loss
+%%%     min --> nonnegative double of the minimum gap for the edge
+%%%     max --> nonnegative double of the maximum gap for the edge
+%%%
+%%%     OPTIONAL INPUT DEFAULTS:
+%%%     parameter = 0
+%%%     penalty = 0
+%%%     K = Inf
+%%%     a = 0
+%%%     min = NaN
+%%%     max = NaN
 %%%
 %%%     OUTPUTS:
-%%%     edge ---> Structure organized for implementation to gfpopGraph.m
+%%%     edge --> Structure organized for implementation to gfpopGraph.m
 %%%
 %%%     EXAMPLE:
 %%%     edge1 = gfpopEdge(0,0,"null",1,0,Inf,0,NaN,NaN);
 %%%
 
-function edge = gfpopEdge(state1,state2,edgeType,parameter,penalty,k,a,min,max)
-    edge = struct("state1",state1,"state2",state2,"type",edgeType,"parameter",parameter, ...
-                  "penalty",penalty,"k",k,"a",a,"min",min,"max",max);
+function edge = gfpopEdge(state1,state2,edgeType,varargin)
+    
+    % Default Values for Optional Inputs
+    p = inputParser;
+    defaultparameter = 0;
+    defaultpenalty = 0;
+    defaultk = Inf;
+    defaulta = 0;
+    defaultmin = NaN;
+    defaultmax = NaN;
+
+    % Checking inputs for required and optional values
+    addRequired(p,'state1',@isstring);
+    addRequired(p,'state2',@isstring);
+    addRequired(p,'edgeType',@isstring);
+    addParameter(p,'parameter',defaultparameter,@isnumeric);
+    addParameter(p,'penalty',defaultpenalty,@isnumeric);
+    addParameter(p,'k',defaultk,@isnumeric);
+    addParameter(p,'a',defaulta,@isnumeric);
+    addParameter(p,'min',defaultmin,@isnumeric);
+    addParameter(p,'max',defaultmax,@isnumeric);
+
+    % Parsing inputs together
+    parse(p,state1,state2,edgeType,varargin{:});
+
+    % Constructing edge struct
+    edge = struct("state1",p.Results.state1,"state2",p.Results.state2,"type",p.Results.edgeType,"parameter",p.Results.parameter, ...
+                  "penalty",p.Results.penalty,"k",p.Results.k,"a",p.Results.a,"min",p.Results.min,"max",p.Results.max);
+
 end
--- gfpopGraph.m
+++ gfpopGraph.m
@@ -1,41 +1,106 @@
 %%% Takes in multiple edges from gfpopEdge.m and combines them into a
 %%% single structure to be implemented into gfpop.m.
 %%%
-%%%     Inputs:
-%%%     varargin --> Takes in a variable number of edges
+%%%     INPUTS:
+%%%     [Required]
+%%%     edges --> vector of edges constructed from gfpopEdge.m
 %%%
-%%%     Outputs:
-%%%     outGraph --> Output structure containing graph of all edges
+%%%     [Optional]
+%%%     startState --> string of the starting state for gfpop.m
+%%%     endState --> string of the ending state for gfpop.m
+%%%     allNullEdges --> boolean for creating a null edge for each state
 %%%
-%%%     Example:
-%%%     exGraph = gfpopGraph(upEdge,dwEdge,upNull,dwNull);
+%%%     OUTPUTS:
+%%%     outputGraph --> Output structure containing graph of all edges
+%%%
+%%%     EXAMPLE:
+%%%     exGraph = gfpopGraph([upEdge dwEdge],startState="up",endState="down",allNullEdges=true);
 %%%     
 
-function [outputGraph,vertexNames] = gfpopGraph(varargin)
+function outputGraph = gfpopGraph(edgesIn,varargin)
+
+    % Setting up optional inputs
+    p = inputParser;
+
+    % Checking inputs for required and optional values
+    addRequired(p,'edgesIn',@isstruct);
+    addParameter(p,'startState',"",@isstring);
+    addParameter(p,'endState',"",@isstring);
+    addParameter(p,'allNullEdges',false,@islogical);
+
+    % Parsing inputs together
+    parse(p,edgesIn,varargin{:});
+    edges = p.Results.edgesIn;
+    startState = p.Results.startState;
+    endState = p.Results.endState;
+    allNullEdges = p.Results.allNullEdges;
+
+    % Declaring vectors
     state1vec = [];
     state2vec = [];
     typevec = [];
-    paramvec = zeros(1,length(varargin));
-    penaltyvec = zeros(1,length(varargin));
-    kvec = zeros(1,length(varargin));
-    avec = zeros(1,length(varargin));
-    minvec = zeros(1,length(varargin));
-    maxvec = zeros(1,length(varargin));
-    for i = 1:length(varargin)
-        tempEdge = varargin{1,i};
+    paramvec = [];
+    penaltyvec = [];
+    kvec = [];
+    avec = [];
+    minvec = [];
+    maxvec = [];
+
+    % Defined edges
+    for i = 1:length(edges)
+        tempEdge = edges(i);
         state1vec = [state1vec tempEdge.state1];
         state2vec = [state2vec tempEdge.state2];
         typevec = [typevec tempEdge.type];
-        paramvec(i) = tempEdge.parameter;
-        penaltyvec(i) = tempEdge.penalty;
-        kvec(i) = tempEdge.k;
-        avec(i) = tempEdge.a;
-        minvec(i) = tempEdge.min;
-        maxvec(i) = tempEdge.max;
+        paramvec = [paramvec tempEdge.parameter];
+        penaltyvec = [penaltyvec tempEdge.penalty];
+        kvec = [kvec tempEdge.k];
+        avec = [avec tempEdge.a];
+        minvec = [minvec tempEdge.min];
+        maxvec = [maxvec tempEdge.max];
     end
-    inputGraph = struct("state1",state1vec,"state2",state2vec,"type",typevec,"parameter",paramvec, ...
-                      "penalty", penaltyvec,"K",kvec,"a",avec,"min",minvec,"max",maxvec);
 
-    [outputGraph,vertexNames] = gfpopGraphReorder(inputGraph);
+    % Adding optional inputs
+    if(allNullEdges)
+        uniqueVectors = unique([state1vec state2vec]);
+        for i = 1:length(uniqueVectors)
+            state1vec = [state1vec uniqueVectors(i)];
+            state2vec = [state2vec uniqueVectors(i)];
+            typevec = [typevec "null"];
+            paramvec = [paramvec 1];
+            penaltyvec = [penaltyvec 0];
+            kvec = [kvec Inf];
+            avec = [avec 0];
+            minvec = [minvec NaN];
+            maxvec = [maxvec NaN];
+        end
+    end
+
+    if(strlength(startState) ~= 0)
+        state1vec = [state1vec startState];
+        state2vec = [state2vec NaN];
+        typevec = [typevec "start"];
+        paramvec = [paramvec NaN];
+        penaltyvec = [penaltyvec NaN];
+        kvec = [kvec NaN];
+        avec = [avec NaN];
+        minvec = [minvec NaN];
+        maxvec = [maxvec NaN];
+    end
+
+    if(strlength(endState) ~= 0)
+        state1vec = [state1vec endState];
+        state2vec = [state2vec NaN];
+        typevec = [typevec "end"];
+        paramvec = [paramvec NaN];
+        penaltyvec = [penaltyvec NaN];
+        kvec = [kvec NaN];
+        avec = [avec NaN];
+        minvec = [minvec NaN];
+        maxvec = [maxvec NaN];
+    end
+
+    outputGraph = struct("state1",state1vec,"state2",state2vec,"type",typevec,"parameter",paramvec, ...
+                   "penalty", penaltyvec,"K",kvec,"a",avec,"min",minvec,"max",maxvec);
 
 end
