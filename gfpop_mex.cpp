de5322de254c297984bc19b630be3c6adcc7afbd
nat-sweeney <100242406+nat-sweeney@users.noreply.github.com>
2022-06-24 09:05:51 -0600

    Ypdated

--- gfpop_mex.cpp
+++ gfpop_mex.cpp
@@ -31,6 +31,7 @@
 #include "mexAdapter.hpp"
 
 using namespace matlab::data;
+using namespace matlab::engine;
 using matlab::mex::ArgumentList;
 
 class MexFunction : public matlab::mex::Function{
@@ -42,12 +43,13 @@ public:
     void operator()(ArgumentList outputs, ArgumentList inputs){
         //checkArguments(outputs, inputs);
 
-        // Input Setup
+        // Input 
+
+        // Data
         TypedArray<double> vectDataIn = std::move(inputs[0]);
         std::vector<double> vectData(vectDataIn.begin(),vectDataIn.end());
-        stream << "data loaded\n";
-        displayOnMATLAB(stream);
-       
+        
+       // Graph
         StructArray mygraph(inputs[1]);
         Range<ForwardIterator,MATLABFieldIdentifier const> fields = mygraph.getFieldNames();
         TypedArray<double> state1In = mygraph[0][fields.begin()[0]];
@@ -61,135 +63,41 @@ public:
         TypedArray<double> maxxIn = mygraph[0][fields.begin()[8]];
 
                   ///9 variables in mygraph
-    stream << "state 1\n";
-    displayOnMATLAB(stream);
         std::vector<int> state1(state1In.begin(),state1In.end());
     
-    stream << "state 2\n";
-    displayOnMATLAB(stream);
         std::vector<int> state2(state2In.begin(),state2In.end());
     
-    stream << "edge type\n";
-    displayOnMATLAB(stream);
         std::vector<std::string> typeEdge(edgeIn.begin(),edgeIn.end());
     
-    stream << "parameter\n";
-    displayOnMATLAB(stream);
         std::vector<double> parameter(parameterIn.begin(),parameterIn.end());
     
-    stream << "penalty\n";
-    displayOnMATLAB(stream);
         std::vector<double> penalty(penaltyIn.begin(),penaltyIn.end());
     
-    stream << "k\n";
-    displayOnMATLAB(stream);
         std::vector<double> KK(KIn.begin(),KIn.end());
     
-    stream << "a\n";
-    displayOnMATLAB(stream);
         std::vector<double> aa(aIn.begin(),aIn.end());
           
-    stream << "min\n";
-    displayOnMATLAB(stream);
         std::vector<double> minn(minnIn.begin(),minnIn.end());
     
-    stream << "max\n";
-    displayOnMATLAB(stream);
         std::vector<double> maxx(maxxIn.begin(),maxxIn.end());
-        
-    stream << "graph loaded\n";
-    displayOnMATLAB(stream);
 
+        // Type
         TypedArray<MATLABString> typeIn = inputs[2];
         std::string type = std::string(typeIn[0]);
-    stream << "type loaded\n";
-    displayOnMATLAB(stream);
 
-
+        // Vector Weights
         TypedArray<double> vectWeightIn = std::move(inputs[3]);
         std::vector<double> vectWeight(vectWeightIn.begin(),vectWeightIn.end());
-    stream << "weights loaded\n";
-    displayOnMATLAB(stream);
 
-        int testMode = 0;
-
-        /* Only useful for debugging
-        TypedArray<bool> testModeIn = std::move(inputs[4]);
-        //std::vector<bool> testMode(testModeIn.begin(),testModeIn.end());
-        std::vector<bool> testMode(testModeIn[0]);
-        stream << "test mode loaded\n";
-        displayOnMATLAB(stream);
-        */
-
-// INPUT CHECKING ON GRAPH AND DATA
-stream << "--- INPUT CHECKING --- INPUT CHECKING --- INPUT CHECKING ---";
-displayOnMATLAB(stream);
-
-int indexCheck = 0;
-
-stream << "\nIndex : ";
-displayOnMATLAB(stream);
-stream << std::to_string(indexCheck);
-displayOnMATLAB(stream);
-
-stream << "\n State1: ";
-displayOnMATLAB(stream);
-stream << std::to_string(state1[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n State2: ";
-displayOnMATLAB(stream);
-stream << std::to_string(state2[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n Edge Type: ";
-displayOnMATLAB(stream);
-stream << (typeEdge[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n Parameter: ";
-displayOnMATLAB(stream);
-stream << std::to_string(parameter[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n Penalty: ";
-displayOnMATLAB(stream);
-stream << std::to_string(penalty[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n K: ";
-displayOnMATLAB(stream);
-stream << std::to_string(KK[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n a: ";
-displayOnMATLAB(stream);
-stream << std::to_string(aa[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n min: ";
-displayOnMATLAB(stream);
-stream << std::to_string(penalty[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n max: ";
-displayOnMATLAB(stream);
-stream << std::to_string(penalty[indexCheck]);
-displayOnMATLAB(stream);
-
-stream << "\n --- END CHECKING --- END CHECKING --- END CHECKING --- \n";
-displayOnMATLAB(stream);
-
-
+        // Test Mode
+        int testMode = std::move(inputs[4][0]);
         
         // Code from main.cpp for gfpop
                 ///////////////////////////////////////////
           /////////// DATA TRANSFORMATION ///////////
           ///////////////////////////////////////////
           double epsilon = std::pow(10.0,-12.0);
-        
-      stream << "checking type\n";
-      displayOnMATLAB(stream);    
+       
           if(type == "variance")
           {
             double mean = 0;
@@ -230,9 +138,6 @@ displayOnMATLAB(stream);
             disp = disp/k;
             for(int i = 0; i < vectData.size(); i++){vectData[i] = vectData[i]/disp; if(vectData[i] == 0){vectData[i] = epsilon/(1- epsilon);}}
           }
-
-    stream << "type checked\n";
-    displayOnMATLAB(stream);
         
           // BEGIN TRANSFERT into C++ objects  // BEGIN TRANSFERT into C++ objects  // BEGIN TRANSFERT into C++ objects
           // BEGIN TRANSFERT into C++ objects  // BEGIN TRANSFERT into C++ objects  // BEGIN TRANSFERT into C++ objects
@@ -242,69 +147,17 @@ displayOnMATLAB(stream);
           /////////// DATA COPY ///////////
           /////////////////////////////////
 
-stream << "data copy\n";
-displayOnMATLAB(stream);
-
           Data data = Data();
           data.copy(vectData, vectWeight, vectData.size(), vectWeight.size());
         
           //////////////////////////////////
           /////////// GRAPH COPY ///////////
           //////////////////////////////////
-
-stream << "graph copy\n";
-displayOnMATLAB(stream);
         
           Graph graph = Graph();
           Edge newedge;
 
-stream << "graph copy loop\n";
-displayOnMATLAB(stream);
-
           for(int i = 0 ; i < state1.size(); i++){
-stream << std::to_string(i);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(state1[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(state2[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << (typeEdge[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(parameter[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(penalty[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(KK[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(aa[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(minn[i]);
-displayOnMATLAB(stream);
-stream << "   |   ";
-displayOnMATLAB(stream);
-stream << std::to_string(maxx[i]);
-displayOnMATLAB(stream);
-
-
-
-stream << "\n";
-displayOnMATLAB(stream);
               graph << Edge(state1[i], state2[i], typeEdge[i], fabs(parameter[i]), penalty[i], fabs(KK[i]), fabs(aa[i]), minn[i], maxx[i]);
           }
 
@@ -316,9 +169,6 @@ displayOnMATLAB(stream);
           /////////////////////////////////////////////
           /////////// COST FUNCTION LOADING ///////////
           /////////////////////////////////////////////
-
-    stream << "cost function loading\n";
-    displayOnMATLAB(stream);
         
           cost_coeff = coeff_factory(type);
           cost_eval = eval_factory(type);
@@ -342,42 +192,9 @@ displayOnMATLAB(stream);
           /////////////////////////////
           /////////// OMEGA ///////////
           /////////////////////////////
-
-stream << "Edge Check\n";
-displayOnMATLAB(stream);
-for(int i = 0; i < graph.nb_edges(); i++){
-    Edge tmp = graph.getEdge(i);
-    stream << std::to_string(i);
-    displayOnMATLAB(stream);
-    stream << ": ";
-    displayOnMATLAB(stream);
-    stream << std::to_string(tmp.getState1());
-    displayOnMATLAB(stream);
-    stream << "\n";
-    displayOnMATLAB(stream);
-}
-
-    stream << "\nomega construction\n";
-    displayOnMATLAB(stream);
         
           Omega omega(graph);
 
-
-
-Point* tmpData = data.getVecPt();
-stream << std::to_string(tmpData[0].y);
-displayOnMATLAB(stream);
-stream << "\n";
-displayOnMATLAB(stream);
-stream << std::to_string(tmpData[0].w);
-displayOnMATLAB(stream);
-stream << "\n";
-displayOnMATLAB(stream);
-
-        
-
-    stream << "omega run\n";
-    displayOnMATLAB(stream);
           if(testMode == 0){
               omega.gfpop(data);
           }else{
@@ -387,9 +204,6 @@ displayOnMATLAB(stream);
           /////////////////////////////
           /////////// RETURN //////////
           /////////////////////////////
-
-    stream << "gathering outputs\n";
-    displayOnMATLAB(stream);
         
         // Gathering Output Parameters into C++ Data
         std::vector<std::vector<int>> changepoints = omega.GetChangepoints();
@@ -398,69 +212,31 @@ displayOnMATLAB(stream);
         std::vector<std::vector<double>> parameters = omega.GetParameters();
         std::vector<double> globalCost = omega.GetGlobalCost();
 
-    stream << "generating output structure\n";
-    displayOnMATLAB(stream);
-
-        // Output Structure Array Setup
+        // Output Vector Creation
         StructArray res = factory.createStructArray({1,1},{"changepoints", "states", "forced", "parameters", "globalcost"});
-        CellArray changepointsCell = factory.createCellArray({1,1});
-        CellArray statesCell = factory.createCellArray({1,1});
-        CellArray forcedCell = factory.createCellArray({1,1});
-        CellArray paramCell = factory.createCellArray({1,1});
-        TypedArray<double> globalCostCell = factory.createArray<double>({1,globalCost.size()}, globalCost.data(), globalCost.data()+globalCost.size());
+        TypedArray<int> changepointsOut = factory.createArray<int>({1,changepoints[0].size()},changepoints[0].data(),changepoints[0].data()+changepoints[0].size());
+        TypedArray<int> statesOut = factory.createArray<int>({1,states[0].size()},states[0].data(),states[0].data()+states[0].size());
+        TypedArray<bool> forcedOut({1,forced[0].size()},forced[0].begin(),forced[0].end());
+        TypedArray<double> paramsOut = factory.createArray<double>({1,parameters[0].size()},parameters[0].data(),parameters[0].data()+parameters[0].size());
+        TypedArray<double> globalCostOut = factory.createArray<double>({1,globalCost.size()}, globalCost.data(), globalCost.data()+globalCost.size());
 
-    stream << "--- OUTPUT CHECKING --- OUTPUT CHECKING --- OUTPUT CHECKING ---\n";
-    displayOnMATLAB(stream);
+        // Reversing Vectors to go Start -> Finish
+        std::reverse(changepointsOut.begin(),changepointsOut.end());
+        std::reverse(statesOut.begin(),statesOut.end());
+        std::reverse(forcedOut.begin(),forcedOut.end());
+        std::reverse(paramsOut.begin(),paramsOut.end());
+        std::reverse(globalCostOut.begin(),globalCostOut.end());
 
-    stream << ("changepoints.size: ");
-    displayOnMATLAB(stream);
-    stream << std::to_string(changepoints.size());
-    displayOnMATLAB(stream);
-    stream << ("\n");
-    displayOnMATLAB(stream);
+        // Assigning to Struct
+        res[0]["changepoints"] = changepointsOut;
+        res[0]["states"] = statesOut;
+        res[0]["forced"] = forcedOut;
+        res[0]["parameters"] = paramsOut;
+        res[0]["globalcost"] = globalCostOut;
 
-    stream << ("states.size: ");
-    displayOnMATLAB(stream);
-    stream << std::to_string(states.size());
-    displayOnMATLAB(stream);
-    stream << ("\n");
-    displayOnMATLAB(stream);
-    
-    stream << ("col  :  state\n");
-    displayOnMATLAB(stream);
-    for(int i = 0; i < changepoints.size(); i++){
-        for(int j = 0; j < changepoints[i].size(); j++){
-            stream << std::to_string(changepoints[i][j]);
-            displayOnMATLAB(stream);
-            stream << " --> ";
-            displayOnMATLAB(stream);
-            stream << std::to_string(states[i][j]);
-            displayOnMATLAB(stream);
-            stream << "\n";
-            displayOnMATLAB(stream);
-        }
-    }
-
-        stream << "--- END OUTPUT CHECKING --- END OUTPUT CHECKING --- END OUTPUT CHECKING ---\n";
-    displayOnMATLAB(stream);
-
-        // Output Structure Array Population
-
-
-    stream << "assigning outputs\n";
-    displayOnMATLAB(stream);    
-
-        // Assigning outputs
-        res[0]["changepoints"] = changepointsCell;
-        res[0]["states"] = statesCell;
-        res[0]["forced"] = forcedCell;
-        res[0]["parameters"] = paramCell;
-        res[0]["globalcost"] = globalCostCell;
-
-        // Output Setup
+        // Sending struct to output
         outputs[0] = res;
-        
-        //return res;
+
     }
     /*
     void checkArguments(ArgumentList outputs, ArgumentList inputs){
@@ -472,6 +248,7 @@ displayOnMATLAB(stream);
     }
     */
 
+    // Printing stream to console
     void displayOnMATLAB(std::ostringstream& stream){
         matlabPtr -> feval(u"fprintf",0,std::vector<Array>({factory.createScalar(stream.str())}));
         stream.str("");
